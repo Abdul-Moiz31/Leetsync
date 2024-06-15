e3a76ce800061ae326385da275f21f96a4d1acb2
Abdul Moiz <115508605+Abdul-Moiz31@users.noreply.github.com>
2024-06-15 15:38:01 +0500

    Time: 0 ms (100%), Space: 10.3 MB (97.95%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // int i=0,j=nums.size()-1;
+        // if(nums.size()==1){
+        //     if(nums[0]==val){
+        //         nums.pop_back();
+        //         return 0;
+        //     }
+        // }
+        // while(i<=j){
+        //     if(nums[i]==val){
+        //         swap(nums[i],nums[j]);
+        //         j--;
+        //         nums.pop_back();
+        //     }
+        //     else i++;
+        // }
+        // return nums.size();
+        int index = 0;
+        for(int i = 0 ; i< nums.size(); i++){
+            if(nums[i] !=val){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+        }
+    };
