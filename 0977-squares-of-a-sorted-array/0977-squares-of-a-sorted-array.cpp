1decc48a2144c9de6737c525bee34c58866f63ca
Abdul Moiz <abdulmoiz3140@gmail.com>
2024-07-31 20:13:30 +0500

    Time: 24 ms (52.65%), Space: 29.8 MB (17.83%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i =0 ; i<nums.size() ; i++) {
-            nums[i] = ( nums[i]* nums[i]);
-        }
-        sort(nums.begin() , nums.end());
-        return nums ; 
+      for ( int i = 0 ; i<nums.size() ; i++){
+        nums[i] = nums[i]*nums[i];
+      }
+      sort(nums.begin(),nums.end());
+      return nums;
     }
 };
- 
 
- 
+// [-4,-1,0,3,10]
+
+//  for(int i=0;i<nums.size();i++){
+// 		nums[i]=nums[i]*nums[i];
+// 	}
+// 	sort(nums.begin(),nums.end());
+//     i=0 = -4  ; 
+//     nums[0]=nums[0]*nums[0];
+//     = -4 * -4 = 16 
+//    i =1 = -1 
+// nums[1]=nums[1]*nums[1];
+//  = -1 * -1 = 1
+// i = 2 = 0 
+// nums[2]=nums[2]*nums[2];
+// 0 * 0 = 0
+// i = 3 = 3
+// nums[3]=nums[3]*nums[3];
+// 3* 3 = 9 
+// i = 4 = 10
+// nums[3]=nums[3]*nums[3];
+// 10* 10 = 100
+// { 16 , 1 , 0 , 9 , 100 } 
+// using namespace std:
+// {1 , 16 , 0 , 9 , 100}
+// { 0 , 1 , 9 , 16  , 100}
