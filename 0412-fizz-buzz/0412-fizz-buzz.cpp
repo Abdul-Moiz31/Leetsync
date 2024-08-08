e1a147db20765b4fcbc93e3ed5783870ed8d8ac6
Abdul Moiz <abdulmoiz3140@gmail.com>
2024-08-09 01:55:23 +0500

    Time: 3 ms (45.35%), Space: 11.7 MB (14.46%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-      vector<string> result(n);
-      for(int i = 1 ; i<n+1 ; i++){
-        if(i%3==0 && i%5==0){
-            result[i-1] = "FizzBuzz";
-        } else if(i%3==0){
-            result[i-1] = "Fizz";
-        } else if(i%5==0){
-            result[i-1] = "Buzz";
-        } else{
-            result[i-1] = to_string(i);
-        }
-      }
-      return result;
+       vector<string> result;
+       for(int i=1 ; i<=n ; i++){
+           if(i%3 == 0 && i%5== 0){
+             result.push_back("FizzBuzz");
+           } else if (i%3 == 0){
+             result.push_back("Fizz");
+           } else if (i%5 == 0){
+           result.push_back("Buzz");
+           } else {
+              result.push_back(to_string(i));
+           }
+       }
+        return result; 
+
     }
 };  
+
+
+// ["1","2","Fizz"]
+// intput = {1 , 2 , 3}
+
+
+
+// Input: n = 3 
+// {"1" , "2" , "3"};
+// i%3 == 0 && i%5 == 0
+// return "FizzBuzz"
+// {"1" , "2" , "Fizz("};
+// (i%3 == 0)
+// return "Fizz"
+// Input: n = 5
+// nums { 1, 2 , 3 , 4 , 5}
+// nums { 1, 2 , "Fizz" , 4 , "Buzz"}
+
+
+
+
