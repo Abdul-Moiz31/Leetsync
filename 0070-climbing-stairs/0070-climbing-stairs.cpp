7e93072ebdaa83028bbab3a9ac009dfdf57b699f
Abdul Moiz <abdulmoiz3140@gmail.com>
2024-09-30 04:42:02 +0500

    Time: 2 ms (48.71%), Space: 7.4 MB (89.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        ios_base::sync_with_stdio(false);cin.tie(NULL);
-        int a=1,b=1;
-        for(int i=1;i<n;i++)
+        int prev=0;
+        int prev2 = 0;
+        int curr;
+        if(n==1)
+            return 1;
+        if(n == 2)
+            return 2;
+        if(n == 3)
+            return 3;
+        prev2 = 2;
+        prev = 3;
+        for(int i = 4;i<=n;i++)
         {
-            b+=a;
-            a=b-a;
+            curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
         }
-        return b;
+        return curr;
+
     }
 };
