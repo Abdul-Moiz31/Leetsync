9a1cf1ccc73ecb5ddb8af7e007fed598d9444687
Abdul Moiz <abdulmoiz3140@gmail.com>
2024-11-16 08:31:33 +0500

    Time: 7 ms (39%), Space: 26.6 MB (6.08%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        map<char,int> row[9],col[9],blk[9];
-        char c;
-        for(int i=0;i<9;i++){
-           for (int j=0;j<9;j++){
-                c = board[i][j];
-                if((c!='.')&&(0<row[i][c]++ || 0<col[j][c]++ || 0<blk[i/3*3+j/3][c]++))
-                return false;
-            }
+        unordered_map<int , unordered_set<char>> rows , cols;
+        map<pair<int,int> , unordered_set<char>> squares;
+
+        for(int r=0 ; r < 9 ; r++ ){
+            for(int c=0 ; c < 9 ; c++ ){
+               if(board[r][c]=='.') continue;
+       
+             pair<int, int> squareKey = {r / 3, c / 3};
+
+                if (rows[r].count(board[r][c]) || cols[c].count(board[r][c]) || squares[squareKey].count(board[r][c])) {
+                    return false;
+                }
+            rows[r].insert(board[r][c]);
+            cols[c].insert(board[r][c]);
+            squares[squareKey].insert(board[r][c]);
         }
-        return true;
     }
+    return true;
+ }
 };
