0aa913d98ffaa474f5001091daf15f7bc2dd34c8
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-04-10 03:18:33 +0500

    Time: 0 ms (100%), Space: 8.7 MB (20.75%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,7 +3,7 @@ public:
     int reverse(int n) {
        long long reverse_num = 0;
        while(n != 0){
-        long long  digit = n % 10;
+        int digit = n % 10;
         n = n/10;
         reverse_num = reverse_num * 10 + digit;
          if (reverse_num < INT_MIN || reverse_num > INT_MAX) {
