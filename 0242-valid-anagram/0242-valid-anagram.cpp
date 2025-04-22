27b4cd5fdaf9cb04b7edf23a2cc8d1a20ce8eda9
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-04-22 18:26:35 +0500

    Time: 3 ms (54%), Space: 9.9 MB (12.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin()  , s.end());
-        sort(t.begin()  , t.end());
-        return s==t;
+        if(s.size() != t.size()) return false;
+
+        unordered_map<int , int> countS, countT;
+        for(int i = 0 ; i<s.size() ; i++){
+            countS[s[i]]++;
+            countT[t[i]]++;
+        }
+        return countS == countT;
 
     }
 };
