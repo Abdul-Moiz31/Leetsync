5ccabc7fe1092355ec04ddb636e6d5b8ea06625d
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-04-24 16:40:04 +0500

    Time: 0 ms (100%), Space: 19.5 MB (32.41%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    /* Brute Force 
-       int n = nums.size();
-       for(int i = 0 ;i<n;i++){
-        for(int j = i+1 ; j<n; j++){
-            if(nums[i] + nums[j] == target){
-                return {i+1 , j+1};
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                return {left+1, right+1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
-
         }
-       }
-       return {};
-       */
-// Two Pointer 
-       int l = 0 , r = nums.size()-1;
 
-       while(l<r){
-        int currsum = nums[l] + nums[r];
-         if (currsum > target ){
-            r--;
-         } else if (currsum < target){
-            l ++;
-         }
-         else{
-            return {l + 1 , r + 1};
-         }
-       }
-       return {};
+        return {};
     }
 };
