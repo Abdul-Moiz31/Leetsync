10a4b94df9c1cd7af93ab2f37813dd6dbc9b19e1
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-04-25 01:32:15 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool validPalindrome(string s) {
-        int  l = 0 ;
-        int r = s.size()-1;
-        while(l<r){
-            if(s[l] != s[r]){
-                int l1 = l+1;
+        int l = 0;
+        int r = s.size() - 1;
+
+        while (l < r) {
+            if (s[l] != s[r]) {
+                int l1 = l + 1;
                 int r1 = r;
-                int l2 =l;
-                int r2 = r-1;
-                while(l1<r1 && s[l1] == s[r1]){
+                int l2 = l;
+                int r2 = r - 1;
+                if (l1 < r1 && s[l1] == s[r1]) {
                     l1++;
                     r1--;
                 }
-                while(l2<r2 && s[l2] == s[r2]){
+               else  if (l2 < r2 && s[l2] == s[r2]) {
                     l2++;
                     r2--;
                 }
-                return l1>=r1 || l2>=r2;
+                return l1 >= r1 || l2 >= r2;
             }
             l++;
             r--;
