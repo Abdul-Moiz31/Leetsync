8748d1fc8cf5562e1a275c8bf2ded662c70d466e
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-04-25 01:45:07 +0500

    Time: 16 ms (66.62%), Space: 45.9 MB (37.65%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin() , people.end());
-        int left = 0 ;
-        int right= people.size() -1;
+        int left = 0;
+        int right = people.size()-1;
         int cnt =0;
-        while(left<=right){
-            if(people[left] + people[right] <= limit){
+        while(left <= right){
+            int sum = people[left] + people[right];
+            if(sum <= limit){
                 left++;
                 right--;
-            }else{
+            } else if( sum > limit){
                 right--;
+            } else{
+                left++;
             }
             cnt++;
         }
