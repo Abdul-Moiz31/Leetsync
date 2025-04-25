22927afc4da7e5388473a999d29bb13e21a6b597
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-04-26 04:35:01 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -4,34 +4,35 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         vector<vector<int>> quad;
-
         for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
             for (int j = i + 1; j < n; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;  
-
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
                 int left = j + 1;
                 int right = n - 1;
-
                 while (left < right) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
-
+                    long long sum =
+                       nums[i] + nums[j] + nums[left] + nums[right];
                     if (sum < target) {
                         left++;
                     } else if (sum > target) {
-                        right--;
+                       right--;
                     } else {
-                        quad.push_back({nums[i], nums[j], nums[left], nums[right]});
-                        while (left < right && nums[left] == nums[left + 1]) left++;  
-                        while (left < right && nums[right] == nums[right - 1]) right--;  
+                        vector<int> temp = {nums[i], nums[j], nums[left],
+                                            nums[right]};
+                        quad.push_back(temp);
+                        while (left < right && nums[left] == nums[left + 1])
+                            left++;
+                        while (left < right && nums[right] == nums[right - 1])
+                            right--;
                         left++;
                         right--;
                     }
                 }
             }
         }
-
         return quad;
     }
 };
