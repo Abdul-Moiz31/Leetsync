e15e6ceeef846c2d53b207aa6922a411740f31d2
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-05-03 07:20:05 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x , n);
+        if (n < 0)
+            return 1 / myPow(x, -n);
+
+        if (n == 0)
+            return 1; // Base
+
+        if (n % 2 == 0) {
+            return myPow(x * x, n / 2);
+        } // evben
+
+        else {
+            return x * myPow(x, n - 1);
+        }
     }
 };
