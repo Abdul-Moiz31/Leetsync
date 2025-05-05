8c854a454ef911e543c181cd5516abcf485a44aa
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-05-06 04:30:02 +0500

    Time: 0 ms (100%), Space: 10.9 MB (34.37%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,18 +11,18 @@
  */
 class Solution {
 public:
-    void inOrder(TreeNode* root , vector<int> &arr){
-        if(root == nullptr){
+    void  inOrder(TreeNode* root , vector<int>& arr){
+        if(root == NULL){
             return;
         }
-        inOrder(root->left, arr);
+        inOrder(root->left , arr);
         arr.push_back(root->val);
-        inOrder(root->right, arr);
+        inOrder(root->right , arr);
+
     }
-    
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> arr;
-        inOrder(root  , arr);
+        inOrder(root ,arr);
         return arr;
     }
 };
