84b6d23db56aa4d89fb55800a46134fd251067ba
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-05-14 04:55:57 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+         auto dist = [](const vector<int>& point) {
+            return point[0] * point[0] + point[1] * point[1];
+        };
+
+        // Max heap to keep k closest points
+        priority_queue<pair<int, vector<int>>> maxHeap;
+
+        for (const auto& point : points) {
+            int d = dist(point);
+            maxHeap.push({d, point});
+            if (maxHeap.size() > k) {
+                maxHeap.pop();  // Remove the farthest
+            }
+        }
+
+        vector<vector<int>> result;
+        while (maxHeap.empty()) {
+            result.push_back(maxHeap.top().second);
+            maxHeap.pop();
+        }
+
+        return result;
+    }
+};
