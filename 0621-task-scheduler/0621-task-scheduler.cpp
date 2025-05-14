fcd8f660c956fc92ecf65ac0a38dc014c19be5d5
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-05-14 06:06:12 +0500

    Time: 5 ms (67.54%), Space: 38.4 MB (69.39%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char, int> freq;
-        for (char task : tasks) {
-            freq[task]++;
+        map<char , int> mp;
+        for(int i = 0 ; i< tasks.size() ; i++){
+            mp[tasks[i]]++;
         }
+        priority_queue<int> pq;
+        for( auto it : mp){
+            pq.push(it.second);
+}
+        int gaps = pq.top() -1;
+        int idle = gaps * n;
+        pq.pop();
 
-        priority_queue<int> maxHeap;
-        for (auto [task, count] : freq) {
-            maxHeap.push(count);
-        }
 
-        int time = 0;
+        while(!pq.empty()){
+            int freq = pq.top();
+            pq.pop();
+            idle -= min(freq , gaps);
+ }  
+            if(idle<0) idle = 0;
 
-        while (!maxHeap.empty()) {
-            int cycle = n + 1;
-            vector<int> temp;
-
-            int tasksProcessed = 0;
-            for (int i = 0; i < cycle && !maxHeap.empty(); ++i) {
-                int count = maxHeap.top(); maxHeap.pop();
-                if (--count > 0) {
-                    temp.push_back(count);
-                }
-                time++;
-                tasksProcessed++;
-            }
-
-            for (int count : temp) {
-                maxHeap.push(count);
-            }
-
-            // Add idle time only if there are tasks left
-            if (!maxHeap.empty()) {
-                time += (cycle - tasksProcessed);
-            }
-        }
-
-        return time;
+            return tasks.size() +  idle;
+          
     }
 };
