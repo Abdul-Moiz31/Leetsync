19b742bcf3efff6d4ea78fe17ecc798cb593199f
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-05-16 07:26:10 +0500

    Time: 0 ms (100%), Space: 8.2 MB (11.72%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
+// Tabulization -> Bottom up
     int fib(int n) {
-        if (n <= 1){
+        if (n <= 1)
             return n;
+        vector<int> dp(n + 1, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return fib(n-2) + fib(n-1);
+        return dp[n];
     }
 };
