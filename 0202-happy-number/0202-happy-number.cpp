8eecce49d62f57db9ab2ae911c52c679bf035496
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-05-18 05:36:19 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int getNext(int n){
         int sum = 0;
-        while( n > 0){
+        if(n > 0){
             int digit = n % 10;
+            n = n/10;
             sum += digit * digit;
-            n /= 10;
+            
         }
         return sum;
     }
@@ -13,10 +14,10 @@ public:
     bool isHappy(int n) {
         int slow = n;
         int fast = getNext(n);
-        while(fast != 1 && slow != fast ){
+        while(fast !=1 && slow != fast){
             slow = getNext(slow);
             fast = getNext(getNext(fast));
         }
-        return fast==1;
+        return fast == 1;
     }
 };
