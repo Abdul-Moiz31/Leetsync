b38db82029bb8e48f9d03ae1b7c1c9853608acd1
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-05-21 01:47:47 +0500

    Time: 0 ms (100%), Space: 11.8 MB (53.4%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
-     // Memoization
-    int uniquePathsWithObstacles1(int i , int j , vector<vector<int>>& obstacleGrid ,  vector<vector<int>> &dp ){
-         if (i < 0 || j < 0 || obstacleGrid[i][j] == 1) {
-            return 0;
+// Tabulization
+    int uniquePathsWithObstacles1(int n, int m,
+                                  vector<vector<int>>& obstacleGrid,
+                                  vector<vector<int>>& dp) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+
+                } else if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+
+                } else {
+
+                    int up = 0;
+                    int left = 0;
+                    if (i > 0)
+                        up = dp[i - 1][j];
+                    if (j > 0)
+                        left = dp[i][j - 1];
+
+                    dp[i][j] = up + left;
+                }
+            }
         }
-        if(i==0 && j==0)
-        return 1;
-        // if(i<0 || j<0)
-        //  return 0;
-         
-         if(dp[i][j] != -1)
-            return dp[i][j];
-         
-         int down = uniquePathsWithObstacles1 (i-1 , j , obstacleGrid, dp);
-         int right = uniquePathsWithObstacles1 (i , j-1 , obstacleGrid, dp);
-
-         return dp[i][j]=down + right;
+        return dp[n - 1][m - 1];
     }
-    
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-         int n = obstacleGrid.size();
-         int m = obstacleGrid[0].size();
-        vector<vector<int>> dp(n , vector<int>(m, -1));
 
-        return uniquePathsWithObstacles1(n-1 , m-1 , obstacleGrid , dp);
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+
+        return uniquePathsWithObstacles1(n, m, obstacleGrid, dp);
     }
 };
