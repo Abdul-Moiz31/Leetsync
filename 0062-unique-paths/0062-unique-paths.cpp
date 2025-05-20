4e95bdba2c5176b1e781be08006f4e20845dd40b
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-05-21 01:02:14 +0500

    Time: 1 ms (24.5%), Space: 9.4 MB (8.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    // Memoization Solution
-    int uniquePaths1(int i, int j, vector<vector<int>> &dp) {
+    int helper(int i, int j, vector<vector<int>>& dp) {
         if (i == 0 && j == 0)
             return 1;
         if (i < 0 || j < 0)
-            return 0;   
-
-        if(dp[i][j] != -1)
+            return 0;
+        if (dp[i][j] != -1)
             return dp[i][j];
-     
-        int up = uniquePaths1(i - 1, j, dp);
-        int left = uniquePaths1(i, j - 1, dp);
-
+        int up = helper((i - 1), j, dp);
+        int left = helper(i, (j - 1), dp);
         return dp[i][j] = up + left;
     }
-    int uniquePaths(int n, int m) {
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-
-        return uniquePaths1(n - 1, m - 1, dp);
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return helper(m - 1, n - 1, dp);
     }
 };
