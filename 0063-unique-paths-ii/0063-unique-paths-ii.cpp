3d186da9a04019b636dc97de523dda866ba7dad0
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-05-22 03:45:34 +0500

    Time: 8 ms (4%), Space: 11.9 MB (28.51%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-// Tabulization
-    int uniquePathsWithObstacles1(int n, int m,
-                                  vector<vector<int>>& obstacleGrid,
-                                  vector<vector<int>>& dp) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
+    int helper(int i, int j, vector<vector<int>>& grid , vector<vector<int>> &dp) {
+        // Out of bounds
+        if (i < 0 || j < 0) return 0;
+        // Obstacle
+        if (grid[i][j] == 1) return 0;
+        // Reached start
+        if (i == 0 && j == 0) return 1;
+        if(dp[i][j] != 0) return dp[i][j];
 
-                } else if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-
-                } else {
-
-                    int up = 0;
-                    int left = 0;
-                    if (i > 0)
-                        up = dp[i - 1][j];
-                    if (j > 0)
-                        left = dp[i][j - 1];
-
-                    dp[i][j] = up + left;
-                }
-            }
-        }
-        return dp[n - 1][m - 1];
+        int up = helper(i - 1, j, grid , dp);
+        int left = helper(i, j - 1, grid , dp);
+        return dp[i][j] = up + left;
     }
 
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int n = obstacleGrid.size();
-        int m = obstacleGrid[0].size();
-
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-
-        return uniquePathsWithObstacles1(n, m, obstacleGrid, dp);
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+         vector<vector<int>> dp(m, vector<int>(n, 0));
+        return helper(m - 1, n - 1, obstacleGrid , dp); // Start from bottom-right
     }
 };
