26a3bf83ecdca978ca1152c082ad1e72b8cf9900
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-06-17 14:17:06 +0500

    Time: 7 ms (6.28%), Space: 41.9 MB (34.73%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n , 1);
-        for (int i = 1; i < n; i++) {
-            ans[i] = ans[i - 1] * nums[i - 1];
+        int totalProduct = 1;
+        int zeroCount = 0;
+        unordered_map<int, int> freqMap;
+
+        for (int num : nums) {
+            freqMap[num]++;
+            if (num == 0) {
+                zeroCount++;
+                continue;
+            }
+            totalProduct *= num;
         }
-        int suf = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            ans[i] *= suf;
-            suf *= nums[i];
+
+        vector<int> result;
+        for (int num : nums) {
+            if (zeroCount > 1) {
+                result.push_back(0);
+            } else if (zeroCount == 1) {
+                result.push_back(num == 0 ? totalProduct : 0);
+            } else {
+                result.push_back(totalProduct / num);
+            }
         }
-        return ans;
+
+        return result;
     }
 };
