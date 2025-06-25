ed16c87f4b2988047d5addbe42eb29781660a0ca
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-06-25 10:29:57 +0500

    Time: 57 ms (32.9%), Space: 31.1 MB (24.94%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        vector<vector<int>> triplets;
-        for (int i = 0; i < n ; i++) {
-            if(i>0 && nums[i] == nums[i-1]) continue;
-            int left = i + 1;
-            int right = n - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum > 0) {
-                    right--;
-                } else if (sum < 0) {
-                    left++;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
                 } else {
-                    vector<int> temp = {nums[i], nums[left], nums[right]};
-                    triplets.push_back(temp);
-                    left++;
-                    right--;
-                    while (left < right && nums[left] == nums[left - 1])
-                        left++;
-                    while (left < right &&  nums[right] == nums[right - 1])
-                        right--;
+                    vector<int> temp{nums[i], nums[j], nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
                 }
             }
         }
-        return triplets;
+        return ans;
     }
 };
