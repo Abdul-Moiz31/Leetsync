bb7d026818ae063973261c13557acbcfb4f6e4e8
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-06-25 10:46:25 +0500

    Time: 11 ms (65.58%), Space: 14.1 MB (27.77%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -4,18 +4,17 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         int closest = nums[0] + nums[1] + nums[2];
-        for (int i = 0; i < n-2; i++) {
-            int left = i + 1;
-            int right = n - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
+        for (int i = 0; i < n - 2; i++){
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
                 if (abs(sum - target) < abs(closest - target)) {
                     closest = sum;
-                }
-                if (sum > target) {
-                    right--;
+                } else if (sum < target) {
+                    j++;
                 } else {
-                    left++;
+                    k--;
                 }
             }
         }
