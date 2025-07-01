b1a991e771da10e6ce48b4bd9371cda99316f59c
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-07-01 22:50:10 +0500

    Time: 8 ms (81.22%), Space: 11.9 MB (53.17%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -8,18 +8,16 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
+    bool hasCycle(ListNode* head) {
         ListNode* slow = head;
         ListNode* fast = head;
-
-        while(fast !=NULL && fast->next != NULL && fast->next->next != NULL){
+        while (fast != NULL && fast->next != NULL) {
             slow = slow->next;
             fast = fast->next->next;
-            if(slow == fast){
+            if (slow == fast) {
                 return true;
             }
         }
-        return false;
-
+        return false;   
     }
 };
