1a5fa4e188d25b06de2dc4b3be70578ba7cfbff1
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-07-18 16:42:05 +0500

    Time: 0 ms (100%), Space: 11.1 MB (96.67%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
-    void solve(string op, int open, int close, vector<string> &ans){
-        if(open == 0 && close == 0){
-            ans.push_back(op);
-            return;
-        }
-        if(open == close){
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
-        }
-        else if(open == 0){
-            string op1 = op;
-            op1.push_back(')');
-            solve(op1, open, close-1, ans);
-        }
-        else if(close == 0){
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
-        }
-        else{
-            string op1 = op;
-            string op2 = op;
-            op1.push_back('(');
-            op2.push_back(')');
-            solve(op1, open-1, close, ans);
-            solve(op2, open, close-1, ans);
-        }
-    }
     vector<string> generateParenthesis(int n) {
-        int open = n;
-        int close = n;
-        vector<string> ans;
-        string op = "";
-        solve(op, open, close, ans);
-        return ans;
+        vector<string> result;
+        stack<tuple<string, int, int>> st;
+
+        st.push({"", n, n});  // (current_string, open_left, close_left)
+
+        while (!st.empty()) {
+            auto [current, open, close] = st.top();
+            st.pop();
+
+            if (open == 0 && close == 0) {
+                result.push_back(current);
+            }
+
+            if (open > 0) {
+                st.push({current + "(", open - 1, close});
+            }
+
+            if (close > open) {
+                st.push({current + ")", open, close - 1});
+            }
+        }
+
+        return result;
     }
 };
