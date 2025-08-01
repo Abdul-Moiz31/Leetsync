f964e61a37786af47a0ead38b3533c5614738220
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-08-02 04:30:41 +0500

    Time: 0 ms (100%), Space: 8.5 MB (36.81%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-    if(n == 1 ) return 1;
-    if(n == 0 ) return 0;
-    int dp[n+1];
-    dp[1] =1;
-    dp[2] =2;
-    for(int i =3 ; i <= n ; i++){
-        dp[i] = dp[i-1] + dp[i-2];
-    }
-    return dp[n];
+    int climb(int n, vector<int>& memo) {
+        if (n <= 1)
+            return 1;
+
+        if (memo[n] != -1)
+            return memo[n];
+
+        return memo[n] = climb(n - 1, memo) + climb(n - 2, memo);
     }
 
+    int climbStairs(int n) {
+        vector<int> memo(n + 1, -1);
+        return climb(n, memo);
+    }
 };
