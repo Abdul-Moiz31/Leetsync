e23f1ad70d9f631acbcb772918b7a3e8c4f47a6c
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-08-07 22:17:43 +0500

    Time: 0 ms (100%), Space: 8.4 MB (11.66%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        if (n <= 1){
-            return n;
-        }
-        return fib(n-2) + fib(n-1);
+        vector<int>dp(n+1 , -1);
+        return helper(n , dp);
+    }
+        int helper(int n, vector<int>& dp) {
+            if (n <= 1)
+                return n;
+            if(dp[n] != -1) return dp[n];
+            
+
+            return dp[n] = helper(n-1 , dp) + helper(n-2 , dp);
     }
 };
