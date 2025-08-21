485e5dace926c2d1af928489c67368f6770038f6
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-08-22 04:48:25 +0500

    Time: 0 ms (100%), Space: 18.1 MB (65.99%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -2,44 +2,47 @@ class Solution {
 public:
     vector<int> findOrder(int V, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(V);
+
+        // Building a tree
         int n = prerequisites.size();
-        for(int i = 0 ; i < n ; i++){
-            int course  = prerequisites[i][0];
-            int prereq  = prerequisites[i][1];
+        for (int i = 0; i < n; i++) {
+            int course = prerequisites[i][0];
+            int prereq = prerequisites[i][1];
             adj[prereq].push_back(course);
         }
-        //Indegree
-        vector<int> indegree(V , 0);
-        for(int i = 0 ; i < V ;i++){
-            for (int j = 0 ; j < adj[i].size(); j++){
+
+        // Check Indegree
+        vector<int> indegree(V, 0);
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < adj[i].size(); j++) {
                 int neighbor = adj[i][j];
                 indegree[neighbor]++;
             }
         }
 
-        // Queue 
+        // queue
         queue<int> q;
-        for(int i = 0 ; i< V ; i++){
-            if(indegree[i] == 0){
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
                 q.push(i);
             }
         }
 
         vector<int> topo;
-        while(!q.empty()){
+        while (!q.empty()) {
             int node = q.front();
             q.pop();
             topo.push_back(node);
 
-            for(int i = 0 ;  i < adj[node].size() ; i++){
-                int neighbor  = adj[node][i];
+            for (int i = 0; i <adj[node].size() ; i++) {
+                int neighbor = adj[node][i];
                 indegree[neighbor]--;
-                if(indegree[neighbor] == 0){
+                if (indegree[neighbor] == 0) {
                     q.push(neighbor);
                 }
             }
         }
-        if(topo.size() != V) return topo;
-        return topo;
+        if(topo.size() == V)     return topo;
+        return {};
     }
 };
