5a0d828fb37dd1cb1e482b3aefba130c1a9060ff
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-08-28 04:41:13 +0500

    Time: 0 ms (100%), Space: 14.1 MB (72.71%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
-    set<vector<int>> s;
     void getcombSum(vector<int>& arr, int i ,  int target , vector<int> &combine , vector<vector<int>> &ans){
 
             if(i== arr.size() || target < 0 ) return ;
-            if(target == 0)
-                if(s.find(combine) == s.end()){
-                    s.insert(combine);
+            if(target == 0){
                 ans.push_back(combine);
                 return;
             }
@@ -14,9 +11,8 @@ public:
             combine.push_back(arr[i]);
 
 
-        getcombSum(arr , i+1 , target -arr[i] , combine , ans  ); // single include
+        getcombSum(arr , i , target -arr[i] , combine , ans  ); // single include
 
-        getcombSum(arr , i , target -arr[i] , combine , ans); // multiple include
         
         combine.pop_back(); // Bactrack
 
@@ -24,6 +20,7 @@ public:
     }
     
     vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+        sort(arr.begin() , arr.end());
         vector<vector<int>> ans;
         vector<int> combine;
 
