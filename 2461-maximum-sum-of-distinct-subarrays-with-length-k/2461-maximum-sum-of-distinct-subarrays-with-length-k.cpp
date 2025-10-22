56343306ec9c538a6ed7d397de4cb00cb6d2e16f
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-10-22 10:58:51 +0500

    Time: 117 ms (30.09%), Space: 99.8 MB (22.11%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         unordered_set<int> window;
+        long long currentSum = 0;
+        int left =0 ;
         long long maxSum = 0;
-        long long currSum = 0;
-        int left = 0;
-        for (int right = 0; right < nums.size(); right++) {
-            while(window.find(nums[right]) != window.end()) {
-                currSum -= nums[left];
+        for(int right = 0 ; right <nums.size() ;right++){
+            while(window.find(nums[right]) != window.end()){
+                currentSum -= nums[left];
                 window.erase(nums[left]);
                 left++;
             }
-
             window.insert(nums[right]);
-            currSum += nums[right];
-
-            if (right - left + 1 == k) {
-                maxSum = max(maxSum, currSum);
-
-                currSum -= nums[left];
-                window.erase(nums[left]);
-                left++;
-            }
+            currentSum += nums[right];
+                
+            if( right - left +1 == k ){
+                maxSum = max(maxSum , currentSum);
+            
+            currentSum -= nums[left];
+             window.erase(nums[left]);
+             left++;
+             }
         }
-
         return maxSum;
     }
 };
