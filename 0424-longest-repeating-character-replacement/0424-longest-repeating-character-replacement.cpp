a48e20a0341ecf25b1ae2a467bcc2a8dea77076a
Abdul Moiz <abdulmoiz3140@gmail.com>
2025-11-11 05:27:40 +0500

    Time: 3 ms (68.75%), Space: 11 MB (39.82%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -3,19 +3,17 @@ public:
     int characterReplacement(string s, int k) {
         int left = 0;
         int maxLen = 0;
-        int maxfre = 0;
-        vector<int> hash(26, 0);
-        for(int right =0 ; right < s.size() ; right++) {
+        int maxFre = 0;
+        vector<int> hash(26 , 0);
+        for(int right = 0 ; right < s.size() ; right++){
             hash[s[right] - 'A']++;
-            maxfre = max(maxfre, hash[s[right] - 'A']);
-            if ((right - left + 1) - maxfre > k) {
+            maxFre = max(maxFre , hash[s[right] - 'A']);
+            if((right - left + 1) - maxFre > k){
                 hash[s[left] - 'A']--;
                 left++;
             }
-
-            maxLen = max(maxLen, right - left + 1);
+            maxLen = max( maxLen , right - left + 1);
         }
-
         return maxLen;
     }
 };
